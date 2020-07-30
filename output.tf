e77ed7541381534ab6a3b197cdd31c7f639bb280
Matheus Moraes <matheusfaria.moraes@gmail.com>
2020-07-30 10:54:13 -0300

    terraform fmt output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,3 @@
-output "cluster_username" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.username
-}
-
-output "cluster_password" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.password
-}
-
 output "get_credentials" {
-    value = "az aks get-credentials --name ${var.cluster_name} --resource-group ${var.resource_group_name} --admin"
+  value = "az aks get-credentials --name ${var.cluster_name} --resource-group ${var.resource_group_name} --admin"
 }
